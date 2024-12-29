98e7988b5d52a8a9945ce5ddbf3c48a0de0c3e4e
Jessica Campbell <jesscampb91@gmail.com>
2024-12-30 00:50:23 +0100

    Improved code for more consistency.

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 function toggleProjectDetails(projectId){
     const details = document.getElementById(projectId);
 
-    if (details.style.display === "block"){
-        details.style.display = "none";
+    if (details.style.display === 'block'){
+        details.style.display = 'none';
     }
     else{
-        details.style.display = "block";
+        details.style.display = 'block';
     }
 }
 
 
 // Weather API
-const apiKey = "211ebcacbe72f8271cad90e28195cf46";
-const weatherCard = document.querySelector(".weather-card");
+const apiKey = '211ebcacbe72f8271cad90e28195cf46';
+const weatherCard = document.querySelector('.weather-card');
 
 
 // Get icon based on weather ID
@@ -21,32 +21,32 @@ function getWeatherIcon(weatherId){
 
     switch (true){
         case (weatherId >= 200 && weatherId < 300):
-            return "⛈️"; // thunderstorm
+            return '⛈️'; // thunderstorm
         case (weatherId >= 300 && weatherId < 400):
-            return "🌦️"; // drizzle
+            return '🌦️'; // drizzle
         case (weatherId >= 500 && weatherId < 600):
-            return "🌧️"; // rain
+            return '🌧️'; // rain
         case (weatherId >= 600 && weatherId < 700):
-            return "❄️"; // snow
+            return '❄️'; // snow
         case (weatherId >= 700 && weatherId < 800):
-            return "🌫️"; // mist
+            return '🌫️'; // mist
         case (weatherId === 800):
-            return "☀️"; // sun
+            return '☀️'; // sun
         case (weatherId >= 801 && weatherId < 810):
-            return "☁️"; // clouds
+            return '☁️'; // clouds
         default:
-            return "🤷‍♂️"; // unknown
+            return '🤷‍♂️'; // unknown
     }
 }
 
 
 function displayError(message){
     
-    const errorDisplay = document.createElement("p");
+    const errorDisplay = document.createElement('p');
     errorDisplay.textContent = message;
-    errorDisplay.classList.add("errorDisplay");
+    errorDisplay.classList.add('error-display');
     
-    weatherCard.textContent = "";
+    weatherCard.textContent = '';
     // weatherCard.style.display = "flex";
     weatherCard.appendChild(errorDisplay);
 }
@@ -61,7 +61,7 @@ async function getWeatherData(){
         const response = await fetch(apiUrl);
 
         if(!response.ok){
-            throw new Error("Could not fetch weather data.");
+            throw new Error('Could not fetch weather data.');
         }
 
         const data = await response.json();
@@ -80,23 +80,23 @@ function displayWeatherInfo(data){
            main: {temp, feels_like}, 
            weather: [{id}]} = data;
 
-    weatherCard.textContent = "";
+    weatherCard.textContent = '';
     // weatherCard.style.display = "flex";
 
-    const cityDisplay = document.createElement("h2");
-    const tempDisplay = document.createElement("p");
-    const feelsLikeDisplay = document.createElement("p");
-    const weatherIcon = document.createElement("p");
+    const cityDisplay = document.createElement('h2');
+    const tempDisplay = document.createElement('p');
+    const feelsLikeDisplay = document.createElement('p');
+    const weatherIcon = document.createElement('p');
 
     cityDisplay.textContent = city;
     tempDisplay.textContent = `${(temp).toFixed()}°C`;
     feelsLikeDisplay.textContent = `Känns som: ${(feels_like).toFixed()}°C`;
     weatherIcon.textContent = getWeatherIcon(id);
 
-    cityDisplay.classList.add("city-display");
-    tempDisplay.classList.add("temp-display");
-    feelsLikeDisplay.classList.add("feels-like-display");
-    weatherIcon.classList.add("weather-icon");
+    cityDisplay.classList.add('city-display');
+    tempDisplay.classList.add('temp-display');
+    feelsLikeDisplay.classList.add('feels-like-display');
+    weatherIcon.classList.add('weather-icon');
 
     weatherCard.appendChild(cityDisplay);
     weatherCard.appendChild(tempDisplay);
